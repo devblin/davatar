a95c75422eeaf51db4d5621bfae1124b8dc8f170
devblin <deepanshu.dhruw@outlook.com>
2021-03-11 00:07:44 +0530

    updated

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-import Davatar from "./src/script";
+class Davatar {
+	constructor(dimension, text) {
+		this.canvas = document.createElement("canvas");
+		this.canvas.setAttribute("width", dimension);
+		this.canvas.setAttribute("height", dimension);
+		this.name = text;
+	}
 
-module.exports = {
+	generate = () => {
+		console.log(this.#imgBgColor);
+		console.log(this.#imgFontColor);
+	};
+
+	#imgFontColor = () => {
+		return 1;
+	};
+
+	#imgBgColor = () => {
+		return 11;
+	};
+}
+
+export default {
 	Davatar,
 };
