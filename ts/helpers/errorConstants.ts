f17858e0ca1d88a8ee783734946c615cf4ee6e7d
Ahmed Eltaher <ahmedheltaher@gmail.com>
2022-10-10 11:02:19 +0200

    a little code refactoring

--- ts/helpers/errorConstants.ts
+++ ts/helpers/errorConstants.ts
@@ -1,6 +1,8 @@
-export const NO_TEXT = "text property null/undefined";
-export const TEXT_LENGTH = "text property contains less than 2 characters";
-export const TEXT_NOT_STRING = "text must be a string";
-export const SIZE_LESS_OR_ZERO = "size can't be less than or zero";
-export const MORE_THAN_MAX = "size can't be more than 512";
-export const SIZE_NAN = "size must be an integer";
+export const ERRORS = {
+	NO_TEXT: "text property null/undefined",
+	TEXT_LENGTH: "text property contains less than 2 characters",
+	TEXT_NOT_STRING: "text must be a string",
+	SIZE_LESS_OR_ZERO: "size can't be less than or zero",
+	MORE_THAN_MAX: "size can't be more than 512",
+	SIZE_NAN: "size must be an integer"
+};
--- ts/helpers/validationFunctions.ts
+++ ts/helpers/validationFunctions.ts
@@ -1,69 +1,50 @@
 import { logger } from "./customLogger";
-import { MORE_THAN_MAX, NO_TEXT, SIZE_LESS_OR_ZERO, SIZE_NAN, TEXT_LENGTH, TEXT_NOT_STRING } from "./errorConstants";
+import { ERRORS } from "./errorConstants";
 
 export function checkImageSize(size: number): boolean {
 	if (size <= 0) {
-		logger(SIZE_LESS_OR_ZERO);
+		logger(ERRORS.SIZE_LESS_OR_ZERO);
 		return false;
 	}
 
 	if (size > 512) {
-		logger(MORE_THAN_MAX);
+		logger(ERRORS.MORE_THAN_MAX);
 		return false;
 	}
 
 	if (isNaN(size)) {
-		logger(SIZE_NAN);
+		logger(ERRORS.SIZE_NAN);
 		return false;
 	}
 
 	return true;
-
-	//const isNumber = Number.isInteger(size);
-	//if (!isNumber) logger(SIZE_NAN);
-	//else {
-	//	if (size <= 0) logger(SIZE_LESS_OR_ZERO);
-	//	else if (size > 512) logger(MORE_THAN_MAX);
-	//	else return true;
-	//}
 }
 
 export function checkImageText(text: string): boolean {
 	if (text == null || text == undefined) {
-		logger(NO_TEXT);
+		logger(ERRORS.NO_TEXT);
 		return false;
 	}
 
 	if (text.length < 2) {
-		logger(TEXT_LENGTH);
+		logger(ERRORS.TEXT_LENGTH);
 		return false;
 	}
 
 	if (typeof text != "string") {
-		logger(TEXT_NOT_STRING);
+		logger(ERRORS.TEXT_NOT_STRING);
 		return false;
 	}
 
 	return true;
-
-	//const isString = typeof text === "string";
-	//if (!text) logger(NO_TEXT);
-	//else {
-	//	if (!isString) logger(TEXT_NOT_STRING);
-	//	else if (text.trim().length < 2) logger(TEXT_LENGTH);
-	//	else return true;
-	//}
 }
 
-export function checkDataUrl(testUrl: string): boolean {
-	let testResult = false;
-	if (!testUrl) return false;
-	let base64regex =
-		/^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/;
-	let splitUrl = testUrl.split(",");
-	let base64Test = base64regex.test(splitUrl[1]);
-	testResult =
-		splitUrl[0] === "data:image/png;base64" && base64Test ? true : false;
+export function checkDataUrl(urlToTest: string): boolean {
+	if (!urlToTest)
+		return false;
 
-	return testResult;
+	const base64regex = /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/;
+	const urlParts = urlToTest.split(",");
+
+	return urlParts[0] === "data:image/png;base64" && base64regex.test(urlParts[1]);
 }
