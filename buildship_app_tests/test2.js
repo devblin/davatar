d42cbb93fd706c846ab344acc3217a0f35598aab
devblin <dhruwxl@gmail.com>
2023-12-01 16:38:41 +0530

    one more tedt

--- buildship_app_tests/test2.js
+++ buildship_app_tests/test2.js
@@ -1,31 +1,28 @@
-const { chromium } = require("playwright");
+async function run({ page }) {
+    await page.getByRole('link', { name: 'expression-editor' }).click();
+    await page.getByRole('button', { name: 'Ship' }).click();
 
-async function run() {
-    const browser = await chromium.launch();
-    const page = await browser.newPage();
-    await page
-        .locator("div:nth-child(2) > .MuiButtonBase-root")
-        .first()
-        .click();
-    await page
-        .getByRole("banner")
-        .locator("div")
-        .filter({ hasText: "Untitled Workflow" })
-        .getByRole("button")
-        .click();
-    await page.getByText("Settings").click();
-    await page.getByLabel("Workflow name").click({ clickCount: 3 });
-    await page.getByLabel("Workflow name").fill("WorkflowToBeDeleted");
-    await page.getByRole("button", { name: "Save" }).click();
-    await page
-        .getByRole("link", { name: "WorkflowToBeDeleted" })
-        .getByRole("button")
-        .click();
-    await page.getByText("Delete", { exact: true }).click();
-    await page.getByPlaceholder("WorkflowToBeDeleted").click();
-    await page
-        .getByPlaceholder("WorkflowToBeDeleted")
-        .fill("WorkflowToBeDeleted");
-    await page.getByRole("button", { name: "Delete" }).click();
-    await browser.close();
+    const url = await page.url()
+
+    const projectId = url?.split("/")?.[4]
+
+    const uniqueId = projectId?.split("-")?.[1]
+
+    const deployedURL = `https://${uniqueId}.buildship.run/expression-editor`
+
+    return deployedURL
+}
+
+async function validate({ page, fetch, expect }) {
+    const url = await run({ page })
+
+    const res = await (await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ array: [1, 2, 3, 4] })
+    })).json()
+
+    expect(res).toBe(["ODD", "EVEN", "ODD", "EVEN"])
 }
