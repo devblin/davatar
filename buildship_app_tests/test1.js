5ffdae5b9f087f17b3fee413c31283a87beaba0f
devblin <dhruwxl@gmail.com>
2023-12-01 16:40:01 +0530

    one more tedt

--- buildship_app_tests/test1.js
+++ buildship_app_tests/test1.js
@@ -1,31 +1,43 @@
-const { chromium } = require("playwright");
+async function run({ page }) {
+    await page.locator('body').click();
+    await page.locator('div:nth-child(2) > .MuiButtonBase-root').first().click();
+    await page.getByRole('link', { name: 'Untitled Workflow' }).getByRole('button').click();
+    await page.getByRole('menuitem', { name: 'Settings' }).click();
+    await page.locator('div').filter({ hasText: 'DisplaySet how this workflow is displayed to usersWorkflow nameUser-facing name ' }).nth(2).click();
+    await page.getByLabel('Workflow name').dblclick();
+    await page.getByLabel('Workflow name').fill('helloworld');
+    await page.getByRole('button', { name: 'Save' }).click();
+    await page.getByRole('button', { name: 'Add Trigger' }).click();
+    await page.getByRole('button', { name: 'Rest API Call' }).click();
+    await page.getByRole('button', { name: 'Add Trigger' }).click();
+    await page.getByPlaceholder('Value').click();
+    await page.getByPlaceholder('Value').fill('/helloworld');
+    await page.locator('.MuiStack-root > div > div > div:nth-child(2) > .MuiStack-root').click();
+    await page.getByRole('button', { name: 'Return' }).click();
+    await page.getByRole('button', { name: 'Add Node' }).click();
+    await page.getByLabel('​', { exact: true }).click();
+    await page.getByRole('option', { name: 'OK (200)' }).click();
+    await page.getByLabel('Value', { exact: true }).click();
+    await page.getByLabel('Value', { exact: true }).fill('helloworld');
+    await page.getByRole('button', { name: 'Ship' }).click();
+    await page.waitForResponse(res => res.url().includes("/deploy-workflow") && res.status() === 200)
 
-async function run() {
-    const browser = await chromium.launch();
-    const page = await browser.newPage();
-    await page
-        .locator("div:nth-child(2) > .MuiButtonBase-root")
-        .first()
-        .click();
-    await page
-        .getByRole("banner")
-        .locator("div")
-        .filter({ hasText: "Untitled Workflow" })
-        .getByRole("button")
-        .click();
-    await page.getByText("Settings").click();
-    await page.getByLabel("Workflow name").click({ clickCount: 3 });
-    await page.getByLabel("Workflow name").fill("WorkflowToBeDeleted");
-    await page.getByRole("button", { name: "Save" }).click();
-    await page
-        .getByRole("link", { name: "WorkflowToBeDeleted" })
-        .getByRole("button")
-        .click();
-    await page.getByText("Delete", { exact: true }).click();
-    await page.getByPlaceholder("WorkflowToBeDeleted").click();
-    await page
-        .getByPlaceholder("WorkflowToBeDeleted")
-        .fill("WorkflowToBeDeleted");
-    await page.getByRole("button", { name: "Delete" }).click();
-    await browser.close();
+    const url = await page.url()
+
+    const projectId = url?.split("/")?.[4]
+
+    const uniqueId = projectId?.split("-")?.[1]
+
+    const deployedURL = `https://${uniqueId}.buildship.run/helloworld`
+
+    return deployedURL
+}
+
+
+async function validate({ page, fetch, expect }) {
+    const data = await run1({ page })
+
+    const res = await (await fetch(data)).text()
+
+    expect(res).toBe("helloworld")
 }
